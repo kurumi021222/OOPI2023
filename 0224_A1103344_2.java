856f27e39e4d0bb8431e6ad0547340adf434cd1b
kurumi021222 <125748627+kurumi021222@users.noreply.github.com>
2023-03-01 16:52:52 +0800

    Add files via upload

---
+++ 0224_A1103344_2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class 0224_A1103344_2{
+    public static void main(String args[]) {
+            int a;
+            Scanner input = new Scanner(System.in);
+            a = input.nextInt();
+            float b=(float)a*9/5+32;
+            System.out.println("攝氏"+a+"為華氏"+b);
+            
+    }
+}
